90016d8f12ac4522f059e0fb521b1a98eb7e9b00
Botoi <99215208+Rune-en@users.noreply.github.com>
2022-02-09 20:34:39 +0200

    am facut botu sa mearga cat de cat

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("main")
+# sa stii sa instalezi python package
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("suntem gata ne cheama")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith("fa"):
+        if message.author.discriminator == "6679":
+            await message.channel.send("UITE VIKTOR FAC SCHEME")
+
+        print(message.author.discriminator)
+        await  message.channel.send("ai fost logat lol iti pic netu")
+
+client.run("AI VREA TU")
+
+
